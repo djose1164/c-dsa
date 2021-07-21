b7ff7e8702dd03c2b8ebf84854308e49840c1274
Jose S. Daniel Victoriano B <djose1164@gmail.com>
2021-07-21 18:43:41 -0400

    Update main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "../include/list.h"
 
+sum(unsigned n)
+{
+	if (n)
+		return n + sum(n - 1);
+	else
+		return n;
+}
 int main()
 {
-	printf("Hello World!\n");
-
+	list_menu();
 	return (0);
 }
